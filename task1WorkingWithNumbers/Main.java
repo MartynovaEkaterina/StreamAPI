1d88d8a00991443574f94d7b87dc6950533a6ff2
Ekaterina Martynova <ekaterina.martynova89@mail.ru>
2022-05-21 00:41:14 +0300

    Домашняя работа

---
+++ task1WorkingWithNumbers/Main.java
@@ -0,0 +1,26 @@
+package task1WorkingWithNumbers;
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> finalList = new ArrayList<>();
+        for (int s : intList) {
+            if (s > 0 && s % 2 == 0) {
+                finalList.add(s);
+            }
+        }
+        Collections.sort(finalList);
+        printList(finalList);
+    }
+
+    public static void printList(List<Integer> list) {
+        for (int i : list) {
+            System.out.println(i);
+        }
+    }
+}
+
+
---
+++ task1WorkingWithNumbers/StreamMain.java
@@ -0,0 +1,15 @@
+package task1WorkingWithNumbers;
+
+
+import java.util.*;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        intList.stream()
+                .filter(s -> s > 0)
+                .filter(s -> s % 2 == 0)
+                .sorted(Comparator.naturalOrder())
+                .forEach(System.out::println);
+    }
+}
